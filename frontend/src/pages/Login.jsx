7c60eea1729904b93cdeb59c3c37acab85b97e0f
Adesina Paul <pauladesina@gmail.com>
2025-04-08 12:54:21 +0100

    complete login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,18 @@
+import pic from "../assets/pic.jpg";
+
+
 import { useState } from "react";
+import { Link } from "react-router-dom"
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   return (
     <>
-      <div className="py-[100px] h-full max-w-[1400px] mx-auto  flex w-full ">
-        <div className=" flex flex-col w-1/2 px-[12px] justify-center items-center ">
-          <form className="w-full border-gray-200 border px-15 rounded-lg shadow-md bg-white ">
-            <div className="flex justify-center text-xl mb-3 font-medium  ">
+      <div className="py-[100px] h-full max-w-[1400px] relative mx-auto  flex w-full ">
+        <div className=" flex w-full px-[12px] justify-start items-center ">
+          <form className="w-full border-gray-200 lg:w-1/2  px-8 sm:px-15 md:px-30 lg:px-15 rounded-lg pb-5 bg-white ">
+            <div className="flex  justify-center text-xl mb-4 font-medium  ">
               StyleNest
             </div>
             <h2 className=" text-2xl font-bold text-center mb-2 ">
@@ -28,7 +32,7 @@ const Login = () => {
                 placeholder="Enter Your Email Address"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="border px-2  py-2 w-full border-gray-400 rounded-md "
+                className="border px-2  py-3 w-full border-gray-400 rounded-md "
                 type="email"
               />
             </div>
@@ -43,11 +47,24 @@ const Login = () => {
                 placeholder="Enter Your Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="border px-2 py-2 w-full border-gray-400 rounded-md "
+                className="border px-2 py-3 w-full border-gray-400 rounded-md "
                 type="password"
               />
             </div>
+
+            <button type="submit" className="w-full bg-black hover:bg-gray-800 text-white cursor-pointer my-6 text-lg  rounded-lg font-semibold p-3  " >
+              Sign In
+            </button>
+            <p className="mt-[-12px] text-md text-center " >
+              Don't have an account?
+              <Link to="/register" className="text-blue-500 ml-1 " >
+                  Register
+              </Link>
+            </p>
           </form>
+          <div className=" hidden absolute top-0 bottom-0 right-0 lg:flex w-1/2  " >
+            <img className="w-full h-full flex-shrink-0 object-cover " src={pic} /> 
+          </div>
         </div>
       </div>
     </>
