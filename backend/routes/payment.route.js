b02752d4e992a5f6b89938f1bac921552d5c5d0b
Matthias-Paul <140503382+Matthias-Paul@users.noreply.github.com>
2025-06-24 15:45:55 -0700

    updates the checkout page

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,17 +1,10 @@
+import express from "express";
+import { webHook, makePayment } from "../controllers/payment.js";
 
-import express from "express"
-import { makePayment, webHook } from "../controllers/payment.js"
-import { verifyUser } from "../middleware/verifyUser.js"
-import {  } from "../middleware/validation.js"
-       
+const router = express.Router();
 
-const router = express.Router()
+router.post("/webhook/paystack", express.raw({ type: 'application/json' }), webHook);
 
+router.post("/pay/init", express.json(), makePayment);
 
-router.post("/webhook/paystack", webHook)          
-router.post("/pay/init", makePayment)    
-
-
-   
-export default router
-
+export default router;
