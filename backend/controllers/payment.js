030ffbb3023b3a3d12db2f3aa05d4667e9c4997d
Matthias-Paul <140503382+Matthias-Paul@users.noreply.github.com>
2025-06-26 01:50:24 -0700

    update

--- backend/controllers/payment.js
+++ backend/controllers/payment.js
@@ -160,7 +160,7 @@ export const webHook = async (req, res) => {
         size: item.size,
         color: item.color,
         quantity: item.quantity,
-        total = parseFloat(group.total);
+        total = parseFloat(group.total)
       });
 
       const price = Number(item.price);
